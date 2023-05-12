9c0c07770cb1e5e36deb94de961148b841477ff4
Gabriel Robinson de Azevedo <19100080@liberato.com.br>
2023-05-11 23:20:57 -0300

    improve splash page

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -8,15 +8,31 @@ class SplashPage extends StatefulWidget {
   State<SplashPage> createState() => _SplashPageState();
 }
 
-class _SplashPageState extends State<SplashPage> {
+class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 1, milliseconds: 500),
+    vsync: this,
+  );
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeIn,
+  );
+  Alignment align = Alignment.centerLeft;
+
   @override
   void initState() {
     super.initState();
     changePage(context);
+    _controller.forward();
   }
 
   void changePage(BuildContext context) {
-    Future.delayed(const Duration(seconds: 2)).then(
+    Future.delayed(const Duration(milliseconds: 100)).then((value) => setState(
+          () {
+            align = Alignment.center;
+          },
+        ));
+    Future.delayed(const Duration(seconds: 2, milliseconds: 500)).then(
         (value) => Navigator.pushReplacementNamed(context, Routes.homePage));
   }
 
@@ -24,8 +40,13 @@ class _SplashPageState extends State<SplashPage> {
   Widget build(BuildContext context) {
     return Material(
       color: Theme.of(context).backgroundColor,
-      child: Center(
-        child: Image.asset('assets/images/masterclass_logo.png'),
+      child: AnimatedAlign(
+        duration: const Duration(seconds: 2),
+        alignment: align,
+        curve: Curves.bounceOut,
+        child: FadeTransition(
+            opacity: _animation,
+            child: Image.asset('assets/images/masterclass_logo.png')),
       ),
     );
   }
